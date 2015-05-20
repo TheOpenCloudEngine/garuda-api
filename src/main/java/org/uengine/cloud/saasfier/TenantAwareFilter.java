a68417abd1291d58c218a45c198d9c5a10a97b88
limhoo2015 <limhoo@uengine.org>
2015-05-20 14:10:00 +0900

    Add filter annotation

--- src/main/java/org/uengine/cloud/saasfier/TenantAwareFilter.java
+++ src/main/java/org/uengine/cloud/saasfier/TenantAwareFilter.java
@@ -8,9 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
-
+@WebFilter(filterName="TenantAwareFilter", urlPatterns={ "/*" })
 public class TenantAwareFilter implements Filter{
 
 	@Override
