feac30aac74b34d81604b2576f2fb7ddd6f76473
limhoo2015 <limhoo@uengine.org>
2015-07-14 10:21:33 +0900

    getSelfServicePortalURL와 getIamageURL 메소드 구현

--- src/main/java/org/oce/garuda/multitenancy/TenantSpecificUtil.java
+++ src/main/java/org/oce/garuda/multitenancy/TenantSpecificUtil.java
@@ -14,10 +14,20 @@ public class TenantSpecificUtil {
 		public void setTenantSpecific(TenantSpecific tenantSpecific) {
 			this.tenantSpecific = tenantSpecific;
 		}
-		
+
+	String metadataServer;
+
+		public String getMetadataServer() {
+			return metadataServer;
+		}
+
+		public void setMetadataServer(String metadataServer) {
+			this.metadataServer = metadataServer;
+		}
+
+
 	String appKey;
-		
-		
+
 		public String getAppKey() {
 			return appKey;
 		}
@@ -35,8 +45,17 @@ public class TenantSpecificUtil {
 		return tenantSpecific.initProcess(TenantContext.getThreadLocalInstance().getTenantId(), getAppKey(), processDefId);
 		
 	}
-	
-	
+
+	public String getSelfServicePortalURL(){
+		return "http://" + TenantContext.getThreadLocalInstance().getTenantId() + "." + getMetadataServer() + "/selfservice/" + getAppKey();
+	}
+
+	public String getIamageURL(String imageMetadatKey){
+		return "http://" +TenantContext.getThreadLocalInstance().getTenantId() + "." + getMetadataServer() + "/tenantImage/" + getAppKey() + "/" + imageMetadatKey;
+	}
+
+
+
 //	public void executeProcess(String instanceId);
 //	public void setProcessVariable(String instanceId, String variableKey, String value);
 //	public String getProcessVariable(String instanceId, String variableKey);
